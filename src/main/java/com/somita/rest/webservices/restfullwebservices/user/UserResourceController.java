6263119c3f00034e22ee0248f32c21e9bf07fcda
ediaz13 <diazemanuel27@gmail.com>
2020-10-21 12:45:39 -0300

    Post Method Return Status 201!!

--- src/main/java/com/somita/rest/webservices/restfullwebservices/user/UserResourceController.java
+++ src/main/java/com/somita/rest/webservices/restfullwebservices/user/UserResourceController.java
@@ -1,8 +1,11 @@
 package com.somita.rest.webservices.restfullwebservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -22,8 +25,15 @@ public class UserResourceController {
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user){
+    public ResponseEntity<Object> createUser(@RequestBody User user){
         User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
 }

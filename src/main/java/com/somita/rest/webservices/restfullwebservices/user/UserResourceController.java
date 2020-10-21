faac2a8f928119b45c5b883f4193d8c5f92c17eb
ediaz13 <diazemanuel27@gmail.com>
2020-10-21 11:46:21 -0300

    Post Method!!

--- src/main/java/com/somita/rest/webservices/restfullwebservices/user/UserResourceController.java
+++ src/main/java/com/somita/rest/webservices/restfullwebservices/user/UserResourceController.java
@@ -1,9 +1,7 @@
 package com.somita.rest.webservices.restfullwebservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +19,11 @@ public class UserResourceController {
     @GetMapping("/users/{id}")
     public User retrieveUser (@PathVariable int id) {
         return service.finOne(id);
+    }
+
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user){
+        User savedUser = service.save(user);
     }
 
 }

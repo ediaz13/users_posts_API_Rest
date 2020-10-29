14657efdbde12285b6e4c5a94d1d853b81a0d4fe
ediaz13 <diazemanuel27@gmail.com>
2020-10-28 23:55:26 -0300

    Implementing Validations!!

--- src/main/java/com/somita/rest/webservices/restfullwebservices/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/somita/rest/webservices/restfullwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.somita.rest.webservices.restfullwebservices.exception;
 
 import com.somita.rest.webservices.restfullwebservices.user.UserNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,12 +25,19 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
         return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler(Exception.class)
+    @ExceptionHandler(UserNotFoundException.class)
     public final ResponseEntity<Object> handleUserNotFoundException
             (UserNotFoundException ex, WebRequest request) throws Exception {
         ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
-
         return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex , HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), "Validation fail", ex.getBindingResult().toString());
+        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 
 
--- src/main/java/com/somita/rest/webservices/restfullwebservices/user/User.java
+++ src/main/java/com/somita/rest/webservices/restfullwebservices/user/User.java
@@ -1,13 +1,18 @@
 package com.somita.rest.webservices.restfullwebservices.user;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 public class User {
 
-    private Integer id;
 
     private String name;
 
+    @Size(min=2, message = "Name should have at least 2 charecters")
+    private Integer id;
+
+    @Past
     private Date birthDate;
 
     public User(Integer id, String name, Date birthDate) {

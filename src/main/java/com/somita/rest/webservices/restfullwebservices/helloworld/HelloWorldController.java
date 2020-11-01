8f396ed61afea7dd1d1b4cd5352cf4b28ae8322a
ediaz13 <diazemanuel27@gmail.com>
2020-11-01 15:22:18 -0300

    Advanced Restful Service Features!!

--- src/main/java/com/somita/rest/webservices/restfullwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/somita/rest/webservices/restfullwebservices/helloworld/HelloWorldController.java
@@ -1,14 +1,21 @@
 package com.somita.rest.webservices.restfullwebservices.helloworld;
 
+import org.apache.tomcat.util.http.parser.AcceptLanguage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
-    //Get
-    //URI - /hello-world
-    //method - "Hello world"
+
+    @Autowired
+    private MessageSource messageSource;
+
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
         return "Hello world";
@@ -22,5 +29,10 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean( String.format("Hello world, %s", name));
+    }
+
+    @GetMapping(path = "/hello-world-internatiolized")
+    public String helloWorldInternatiolized(@RequestHeader(name = "Accept-Language", required=true) Locale locale) {
+        return messageSource.getMessage("good.morning.message", null,  locale);
     }
 }

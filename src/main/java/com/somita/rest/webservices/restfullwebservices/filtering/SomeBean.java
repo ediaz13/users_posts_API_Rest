d2acc84a84ec8a757e17d690afa6a82be8f352b9
ediaz13 <diazemanuel27@gmail.com>
2020-11-23 17:57:56 -0300

    Dinamic filtering.

--- src/main/java/com/somita/rest/webservices/restfullwebservices/filtering/SomeBean.java
+++ src/main/java/com/somita/rest/webservices/restfullwebservices/filtering/SomeBean.java
@@ -1,7 +1,9 @@
 package com.somita.rest.webservices.restfullwebservices.filtering;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+@JsonFilter("SomeBeanFilter")
 public class SomeBean {
     private String field1;
     private String field2;
